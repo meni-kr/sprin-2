8dba92a39bba15dfce627bad1a6f88558e3a3af9
meni-kr <meni.kramer@gmail.com>
2024-02-28 15:19:15 +0200

    Create a memeController, code a function renderMeme()

--- js/memeController.js
+++ js/memeController.js
@@ -1,42 +1,56 @@
 'use strict'
 
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]
-var gMeme = {
- selectedImgId: 5,
- selectedLineIdx: 0,
- lines: [
- {
- txt: 'I sometimes eat Falafel',
- size: 20,
- color: 'red'
- }
- ]
+let gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+let gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
 }
-var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
-const gElCanvas = document.querySelector('canvas')
-const gCtx = gElCanvas.getContext('2d')
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+let gElCanvas
+let gCtx
 
 
+function onInit() {
+     gElCanvas = document.querySelector('canvas')
+     gCtx = gElCanvas.getContext('2d')
+     renderMeme(gImgs)
 
-function onInit(){
-    renderMeme()
-}
-
-function renderMeme(){
     
-    drawText('hay',100,100)
-    drawImg(gImgs)
 }
 
-
-
-
-function drawImg([{url}]) {
-    
+function renderMeme([{url}]) {
     const elImg = new Image()
     elImg.src = url
-    elImg.onload = () => 
+    elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText('thats it!',200,200)
+    }
+    
+    
+    
+}
 
-       
+
+
+function drawText(text, x, y) {
+    gCtx.beginPath()
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'orange'
+
+    
+
+    gCtx.font = '35px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
 }
