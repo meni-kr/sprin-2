9793cd53db7c9bdffa32fb24ffc9c3c8ecf307e5
meni-kr <meni.kramer@gmail.com>
2024-02-28 15:49:47 +0200

    Add a memeService and a function getMeme

--- js/memeController.js
+++ js/memeController.js
@@ -1,17 +1,7 @@
 'use strict'
 
 let gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
-let gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I sometimes eat Falafel',
-            size: 20,
-            color: 'red'
-        }
-    ]
-}
+
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
 let gElCanvas
@@ -21,17 +11,18 @@ let gCtx
 function onInit() {
      gElCanvas = document.querySelector('canvas')
      gCtx = gElCanvas.getContext('2d')
-     renderMeme(gImgs)
+     renderMeme()
 
     
 }
 
-function renderMeme([{url}]) {
+function renderMeme() {
+    let meme = getMeme()
     const elImg = new Image()
-    elImg.src = url
+    elImg.src = `img/${meme.selectedImgId}.jpg`
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
-        drawText('thats it!',200,200)
+        drawText(meme.lines[meme.selectedLineIdx].txt,200,200)
     }
     
     
---
+++ js/service/memeService.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let gMeme = {
+    selectedImgId: 7,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
+
+function getMeme(){
+    return gMeme
+}
